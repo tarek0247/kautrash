5c108200a5f734ba619ede123d8a3d06778662e8
tarek0247 <tarekalbakri56@gmail.com>
2026-09-13 23:15:54 +0300

    Update index route with ReportIssue

--- src/router.tsx
+++ src/router.tsx
@@ -4,5 +4,5 @@ import { AppErrorComponent } from "@/lib/error-component";
 import { routeTree } from "./routeTree.gen";
 
 export function getRouter() {
-  return createRouter({ routeTree, defaultErrorComponent: AppErrorComponent });
+  return createRouter({ routeTree, defaultErrorComponent: AppErrorComponent })
 }
